8f31e7e5e316fe58303e55b2a415ae52f7e8a0ad
AwesomeNinja886 <awesomeninja886@gmail.com>
2022-11-17 23:23:16 -0500

    fixed placement logic but collision is broken

--- src/main/java/com/awesomeninja/assorted_additions/block/custom/ChiseledPillarBlock.java
+++ src/main/java/com/awesomeninja/assorted_additions/block/custom/ChiseledPillarBlock.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
     
 public class ChiseledPillarBlock extends Block {
@@ -22,9 +23,26 @@ public class ChiseledPillarBlock extends Block {
     public static final BooleanProperty TOP = BooleanProperty.create("top");
     public static final BooleanProperty BOTTOM = BooleanProperty.create("bottom");
 
-    protected static final VoxelShape CENTER_SHAPE_X = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 16.0D, 14.0D);
     protected static final VoxelShape CENTER_SHAPE_Y = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 16.0D, 14.0D);
-    protected static final VoxelShape CENTER_SHAPE_Z = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 16.0D, 14.0D);
+    protected static final VoxelShape TOP_SHAPE_Y = Block.box(0.0D, 10.0D, 0.0D, 16.0D, 6.0D, 16.0D);
+    protected static final VoxelShape BOTTOM_SHAPE_Y = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D);
+    protected static final VoxelShape BOTH_SHAPE_Y = Shapes.or(TOP_SHAPE_Y, Shapes.or(CENTER_SHAPE_Y, BOTTOM_SHAPE_Y));
+    protected static final VoxelShape TOP_ONLY_SHAPE_Y = Shapes.or(CENTER_SHAPE_Y, TOP_SHAPE_Y);
+    protected static final VoxelShape BOTTOM_ONLY_SHAPE_Y = Shapes.or(CENTER_SHAPE_Y, BOTTOM_SHAPE_Y);
+
+    protected static final VoxelShape CENTER_SHAPE_X = Block.box(0.0D, 2.0D, 2.0D, 16.0D, 14.0D, 14.0D);
+    protected static final VoxelShape TOP_SHAPE_X = Block.box(10.0D, 0.0D, 0.0D, 6.0D, 16.0D, 16.0D);
+    protected static final VoxelShape BOTTOM_SHAPE_X = Block.box(0.0D, 0.0D, 0.0D, 6.0D, 16.0D, 16.0D);
+    protected static final VoxelShape BOTH_SHAPE_X = Shapes.or(TOP_SHAPE_X, Shapes.or(CENTER_SHAPE_X, BOTTOM_SHAPE_X));
+    protected static final VoxelShape TOP_ONLY_SHAPE_X = Shapes.or(CENTER_SHAPE_X, TOP_SHAPE_X);
+    protected static final VoxelShape BOTTOM_ONLY_SHAPE_X = Shapes.or(CENTER_SHAPE_X, BOTTOM_SHAPE_X);
+
+    protected static final VoxelShape CENTER_SHAPE_Z = Block.box(2.0D, 2.0D, 0.0D, 14.0D, 14.0D, 16.0D);
+    protected static final VoxelShape TOP_SHAPE_Z = Block.box(0.0D, 0.0D, 10.0D, 16.0D, 16.0D, 6.0D);
+    protected static final VoxelShape BOTTOM_SHAPE_Z = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 6.0D);
+    protected static final VoxelShape BOTH_SHAPE_Z = Shapes.or(TOP_SHAPE_Z, Shapes.or(CENTER_SHAPE_Z, BOTTOM_SHAPE_Z));
+    protected static final VoxelShape TOP_ONLY_SHAPE_Z = Shapes.or(CENTER_SHAPE_Z, TOP_SHAPE_Z);
+    protected static final VoxelShape BOTTOM_ONLY_SHAPE_Z = Shapes.or(CENTER_SHAPE_Z, BOTTOM_SHAPE_Z);
 
     public ChiseledPillarBlock(Properties p_55926_) {
         super(p_55926_);
@@ -32,7 +50,38 @@ public class ChiseledPillarBlock extends Block {
     }
 
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return pState.getValue(AXIS) == Direction.Axis.X ? CENTER_SHAPE_X : pState.getValue(AXIS) == Direction.Axis.Z ? CENTER_SHAPE_Z : CENTER_SHAPE_Y;
+        if (pState.getValue(AXIS) == Direction.Axis.X) {
+            if (pState.getValue(TOP) && pState.getValue(BOTTOM)) {
+                return BOTH_SHAPE_X;
+            } else if (pState.getValue(TOP) && !pState.getValue(BOTTOM)) {
+                return TOP_ONLY_SHAPE_X;
+            } else if (!pState.getValue(TOP) && pState.getValue(BOTTOM)) {
+                return BOTTOM_ONLY_SHAPE_X;
+            } else {
+                return CENTER_SHAPE_X;
+            }
+        } else if (pState.getValue(AXIS) == Direction.Axis.Y) {
+            if (pState.getValue(TOP) && pState.getValue(BOTTOM)) {
+                return BOTH_SHAPE_Y;
+            } else if (pState.getValue(TOP) && !pState.getValue(BOTTOM)) {
+                return TOP_ONLY_SHAPE_Y;
+            } else if (!pState.getValue(TOP) && pState.getValue(BOTTOM)) {
+                return BOTTOM_ONLY_SHAPE_Y;
+            } else {
+                return CENTER_SHAPE_Y;
+            }
+        } else if (pState.getValue(AXIS) == Direction.Axis.Z) {
+            if (pState.getValue(TOP) && pState.getValue(BOTTOM)) {
+                return BOTH_SHAPE_Z;
+            } else if (pState.getValue(TOP) && !pState.getValue(BOTTOM)) {
+                return TOP_ONLY_SHAPE_Z;
+            } else if (!pState.getValue(TOP) && pState.getValue(BOTTOM)) {
+                return BOTTOM_ONLY_SHAPE_Z;
+            } else {
+                return CENTER_SHAPE_Z;
+            }
+        }
+        return BOTH_SHAPE_Y;
     }
 
     public boolean useShapeForLightOcclusion(BlockState pState) {
@@ -75,59 +124,63 @@ public class ChiseledPillarBlock extends Block {
     }
   
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        BlockState toReturn = this.defaultBlockState();
-        toReturn.setValue(AXIS, pContext.getClickedFace().getAxis());
+        Direction.Axis pAxis = pContext.getClickedFace().getAxis();
 
         Level pLevel = pContext.getLevel();
         BlockPos pPos = pContext.getClickedPos();
-        BlockPos above = pPos.above();
-        BlockPos below = pPos.below();
-        if (pContext.getClickedFace().getAxis() == Direction.Axis.Y) {
-            above = pPos.above();
-            below = pPos.below();
-        } else if (pContext.getClickedFace().getAxis() == Direction.Axis.X) {
-            above = pPos.east();
-            below = pPos.west();
-        } else if (pContext.getClickedFace().getAxis() == Direction.Axis.Z) {
-            above = pPos.south();
-            below = pPos.north();
-        }
+        BlockPos above = getAbove(pPos, pAxis);
+        BlockPos below = getBelow(pPos, pAxis);
         BlockState blockAbove = pLevel.getBlockState(above);
         BlockState blockBelow = pLevel.getBlockState(below);
-        if (blockAbove.getBlock() instanceof ChiseledPillarBlock && blockAbove.getValue(AXIS) == toReturn.getValue(AXIS)) {
-            toReturn.setValue(BOTTOM, false);
+        boolean hasBottom = true;
+        boolean hasTop = true;
+        if (blockAbove.getBlock() instanceof ChiseledPillarBlock && blockAbove.getValue(AXIS) == pAxis) {
+            hasTop = false;
         }
-        if (blockBelow.getBlock() instanceof ChiseledPillarBlock && blockBelow.getValue(AXIS) == toReturn.getValue(AXIS)) {
-            toReturn.setValue(TOP, false);
+        if (blockBelow.getBlock() instanceof ChiseledPillarBlock && blockBelow.getValue(AXIS) == pAxis) {
+            hasBottom = false;
         }
-        return toReturn;
+        return this.defaultBlockState().setValue(AXIS, pAxis).setValue(BOTTOM, hasBottom).setValue(TOP, hasTop);
     }
 
     @Override
     public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
-        BlockState toReturn = pState;
-        BlockPos above = pCurrentPos.above();
-        BlockPos below = pCurrentPos.below();
-        if (pFacing.getAxis() == Direction.Axis.Y) {
-            above = pCurrentPos.above();
-            below = pCurrentPos.below();
-        } else if (pFacing.getAxis() == Direction.Axis.X) {
-            above = pCurrentPos.east();
-            below = pCurrentPos.west();
-        } else if (pFacing.getAxis() == Direction.Axis.Z) {
-            above = pCurrentPos.south();
-            below = pCurrentPos.north();
+        BlockPos above = getAbove(pCurrentPos, pState.getValue(AXIS));
+        BlockPos below = getBelow(pCurrentPos, pState.getValue(AXIS));
+        BlockState blockAbove = pLevel.getBlockState(above);
+        BlockState blockBelow = pLevel.getBlockState(below);
+        boolean hasBottom = true;
+        boolean hasTop = true;
+        if (blockAbove.getBlock() instanceof ChiseledPillarBlock && blockAbove.getValue(AXIS) == pState.getValue(AXIS)) {
+            hasTop = false;
         }
-        if (pLevel.getBlockState(above).getBlock() instanceof ChiseledPillarBlock && pLevel.getBlockState(above).getValue(AXIS) == toReturn.getValue(AXIS)) {
-            toReturn.setValue(BOTTOM, false);
+        if (blockBelow.getBlock() instanceof ChiseledPillarBlock && blockBelow.getValue(AXIS) == pState.getValue(AXIS)) {
+            hasBottom = false;
+        }
+        return pState.setValue(BOTTOM, hasBottom).setValue(TOP, hasTop);
+    }
+
+    private static BlockPos getAbove(BlockPos pInitialPos, Direction.Axis pAxis) {
+        if (pAxis == Direction.Axis.Y) {
+            return pInitialPos.above();
+        } else if (pAxis == Direction.Axis.X) {
+            return pInitialPos.east();
+        } else if (pAxis == Direction.Axis.Z) {
+            return pInitialPos.north();
         } else {
-            toReturn.setValue(BOTTOM, true);
+            return pInitialPos.above();
         }
-        if (pLevel.getBlockState(below).getBlock() instanceof ChiseledPillarBlock && pLevel.getBlockState(below).getValue(AXIS) == toReturn.getValue(AXIS)) {
-            toReturn.setValue(TOP, false);
+    }
+
+    private static BlockPos getBelow(BlockPos pInitialPos, Direction.Axis pAxis) {
+        if (pAxis == Direction.Axis.Y) {
+            return pInitialPos.below();
+        } else if (pAxis == Direction.Axis.X) {
+            return pInitialPos.west();
+        } else if (pAxis == Direction.Axis.Z) {
+            return pInitialPos.south();
         } else {
-            toReturn.setValue(TOP, true);
+            return pInitialPos.below();
         }
-        return toReturn;
     }
 }
